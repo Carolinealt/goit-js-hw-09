f13b3c4c234c517fd124892cc2e44d39e48eb8c7
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-03-21 13:05:08 +0200

    3

---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  emailForm: document.querySelector('[name="email"]'),
+  messageForm: document.querySelector('[name="message"]'),
+};
+const { form, emailForm, messageForm } = refs;
+
+const savedFields = localStorage.getItem('feedback-form-state');
+const parcedJSON = savedFields ? JSON.parse(savedFields) : {};
+
+emailForm.value = parcedJSON.email || '';
+messageForm.value = parcedJSON.message || '';
+const updatedData = {
+  email: emailForm.value.trim(),
+  message: messageForm.value.trim(),
+};
+
+form.addEventListener('input', e => {
+  if (e.target === emailForm || e.target === messageForm) {
+    updatedData[e.target.name] = e.target.value.trim();
+  }
+
+  localStorage.setItem(`feedback-form-state`, JSON.stringify(updatedData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const email = emailForm.value.trim(); // видаляємо пробіли по краях
+  const message = messageForm.value.trim(); // видаляємо пробіли по краях
+
+  if (email && message) {
+    const obj = { email, message };
+    console.log(obj);
+    form.reset();
+    localStorage.clear();
+  } else {
+    console.log('Будь ласка, заповніть усі поля форми.');
+  }
+});
